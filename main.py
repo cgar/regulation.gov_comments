696318d4e1b8361e074814cf314d09c6ff3a594b
Carlos E. Garcia <carlos@cgarcia.org>
2017-02-07 21:46:51 -0500

    pretier url

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def getComments(SECRET_KEY):
     # Call the URL and get the number of comments based on the following
     # criteria: results per page = 100, sort order = DESCending,
     # sort by = Posted Date and keyword = florida
-    commentsURL = "https://api.data.gov/regulations/v3/documents.json?api_key="+SECRET_KEY+"&rpp=100&so=DESC&sb=postedDate&s=florida"
-    response = requests.get(commentsURL)
+    urlPartOne = "https://api.data.gov/regulations/v3/documents.json?api_key="
+    urlPartTwo = "&rpp=100&so=DESC&sb=postedDate&s=florida"
+    url = urlPartOne + SECRET_KEY + urlPartTwo
+    response = requests.get(url)
     response.raise_for_status()
     commentData = json.loads(response.text)
     with open('comments.json', 'w') as fp:

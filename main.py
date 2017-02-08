86613c5f6939b48b166952f0c2f65b0305e8e228
Carlos E. Garcia <carlos@cgarcia.org>
2017-02-07 21:37:50 -0500

    json dumps to file

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from settings import SECRET_KEY
 
 
+import os
 import json
 import requests
 
 
-def getLastComments(SECRET_KEY):
+def getComments(SECRET_KEY):
     # Call the URL and get the number of comments based on the following
     # criteria: results per page = 100, sort order = DESCending,
     # sort by = Posted Date and keyword = florida
-    commentsURL = "https://api.data.gov/regulations/v3/documents.json?api_key="+SECRET_KEY+"&rpp=100&so=DESC&sb=postedDate&s=florida&sp=O"
+    commentsURL = "https://api.data.gov/regulations/v3/documents.json?api_key="+SECRET_KEY+"&rpp=100&so=DESC&sb=postedDate&s=florida"
     response = requests.get(commentsURL)
     response.raise_for_status()
     commentData = json.loads(response.text)
-    print(commentData)
+    with open('comments.json', 'w') as fp:
+        json.dump(commentData, fp)
+    fp.close()
 
 
-getLastComments(SECRET_KEY)
+getComments(SECRET_KEY)
